c0590aa33b20a503525c3d2404d869bff1f4665b
Viktor Stanchev <me@viktorstanchev.com>
2012-02-04 19:48:56 -0800

    added socket.io stuff

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require('express')
   , stylus = require('stylus')
+  , MemoryStore = express.session.MemoryStore
   , io = require('socket.io');
 
 var app = express.createServer();
+var sessionStore = new MemoryStore();
+var Session = require('connect').middleware.session.Session;
 
 app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.cookieParser());
-  app.use(express.session({secret: 'secret', key: 'express.sid'}));
-  /*app.use(function (req, res) {
-    res.end('<h2>Hello, your session id is ' + req.sessionID + '</h2>');
-  });*/
+  app.use(express.session({store: sessionStore
+      , secret: 'secret'
+      , key: 'tj54u49tpowe548tuwp94t58u3w094ity897y8y'}));
   app.use(stylus.middleware(
     { src: __dirname + '/stylus',
       dest: __dirname + '/public'}
@@ -33,26 +35,63 @@ app.listen(port, function() {
 });
 
 var sio = io.listen(app);
- 
-sio.sockets.on('connection', function (socket) {
-    console.log('A socket connected!');
-});
 
 var parseCookie = require('connect').utils.parseCookie;
- 
+
 sio.set('authorization', function (data, accept) {
-    // check if there's a cookie header
     if (data.headers.cookie) {
-      // if there is, parse the cookie
-      data.cookie = parseCookie(data.headers.cookie);
-      // note that you will need to use the same key to grad the
-      // session id, as you specified in the Express setup.
-      data.sessionID = data.cookie['express.sid'];
+        data.cookie = parseCookie(data.headers.cookie);
+        data.sessionID = data.cookie['tj54u49tpowe548tuwp94t58u3w094ity897y8y'];
+        // save the session store to the data object 
+        // (as required by the Session constructor)
+        data.sessionStore = sessionStore;
+        sessionStore.get(data.sessionID, function (err, session) {
+            if (err || !session) {
+                accept('Error', false);
+            } else {
+                // create a session object, passing data as request and our
+                // just acquired session data
+                data.session = new Session(data, session);
+                accept(null, true);
+            }
+        });
     } else {
-       // if there isn't, turn down the connection with a message
-       // and leave the function.
        return accept('No cookie transmitted.', false);
     }
-    // accept the incoming connection
-    accept(null, true);
+});
+
+sio.sockets.on('connection', function (socket) {
+  
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+
+  var hs = socket.handshake;
+  console.log('A socket with sessionID ' + hs.sessionID 
+      + ' connected!');
+  // setup an inteval that will keep our session fresh
+  var intervalID = setInterval(function () {
+      // reload the session (just in case something changed,
+      // we don't want to override anything, but the age)
+      // reloading will also ensure we keep an up2date copy
+      // of the session with our connection.
+      hs.session.reload( function () { 
+          // "touch" it (resetting maxAge and lastAccess)
+          // and save it back again.
+          hs.session.touch().save();
+      });
+  }, 60 * 1000);
+  
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+  
+  socket.on('disconnect', function () {
+      console.log('A socket with sessionID ' + hs.sessionID 
+          + ' disconnected!');
+      // clear the socket interval to stop refreshing the session
+      clearInterval(intervalID);
+  });
 });
