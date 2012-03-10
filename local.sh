20f34e44585efe02debfbeb5d6c3fa23599e402a
Viktor Stanchev <me@viktorstanchev.com>
2012-03-10 12:49:08 -0800

    auto start redis, changed layout to be more fluid

--- local.sh
+++ local.sh
@@ -6,4 +6,12 @@ else
 echo "node inspector already running"
 fi
 
+redis_found=`ps -ef|grep -v grep|grep redis-server|awk '{print $9}'`
+if test -z $redis_found 
+then
+redis-server 2>/dev/null > /dev/null &
+else
+echo "node inspector already running"
+fi
+
 NODE_ENV=development NODE_PORT=3000 NODE_REDIS=local nodemon --debug server.js
