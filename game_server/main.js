0f08941f08e9cdce21e46d9e3882b048ca0a77db
Viktor Stanchev <me@viktorstanchev.com>
2012-06-18 00:28:00 -0400

    started working on express3 transition
    everyauth is not compatible yet, so we have to wait for it

--- game_server/main.js
+++ game_server/main.js
@@ -20,7 +20,23 @@ var rematches = {};
 
 module.exports = {
     onconnect: function(socket) {
+        var hs = socket.handshake;
+        console.log('A socket with sessionID '+hs.sessionID+' connected.');
 
+        /* NOTE: At this point, you win. You can use hs.sessionID and
+        * hs.session. */
+
+        /* NOTE: This function could end here, and everything would be fine.
+        * However, I included this additional mechanism that Daniel
+        * added to keep the session alive by pinging it every 60
+        * seconds. I don't know how useful this is in the context of
+        * this demo, considering that the sessions aren't going to
+        * expire in the near future. So feel free to not include this: */
+        var intervalID = setInterval(function(){
+            hs.session.reload(function(){
+                hs.session.touch().save();
+            });
+        }, 60 * 1000);
 
         socket.on('create', function(name, x, y, cb) {
             //we don't currently support anonymous users
@@ -321,21 +337,6 @@ module.exports = {
         socket.on('list_games', function(cb) {
             cb(db.GameAds.open_games());
         });
-
-        var hs = socket.handshake;
-        console.log('A socket with sessionID ' + hs.sessionID + ' connected!');
-        // setup an inteval that will keep our session fresh
-        var intervalID = setInterval(function() {
-            // reload the session (just in case something changed,
-            // we don't want to override anything, but the age)
-            // reloading will also ensure we keep an up2date copy
-            // of the session with our connection.
-            hs.session.reload(function() {
-                // "touch" it (resetting maxAge and lastAccess)
-                // and save it back again.
-                hs.session.touch().save();
-            });
-        }, 60 * 1000);
 
         function quit() {
             //check if there is a game in an unfinished state left behind and if there is, kill it
--- server.js
+++ server.js
@@ -3,21 +3,23 @@ var express = require('express'),
    io = require('socket.io'),
    RedisStore = require('connect-redis')(express),
    db = require(__dirname + '/db.js'),
-   Session = require('connect').middleware.session.Session,
-   parseCookie = require('connect').utils.parseCookie,
+   connect = require('connect'),
+   cookie = require('cookie'),
    game_server = require(__dirname + '/game_server/main.js'),
    auth = require(__dirname + '/auth.js');
 
+var Session = connect.middleware.session.Session;
 var app = express.createServer();
 auth.init(app);
 
+var secret = 'tj54u49tpowe548tuwp94t58u3w094ity897y8y';
 var sessionStore = new RedisStore({client:db.client});
 
 app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.cookieParser());
   app.use(express.session({store: sessionStore,
-        secret: 'tj54u49tpowe548tuwp94t58u3w094ity897y8y',
+        secret: secret,
         key: 'express.sid'}));
   app.use(express.bodyParser());
   //app.use(express.csrf()); //TODO: USE THIS!!
@@ -43,25 +45,43 @@ app.listen(port, function() {
 var sio = io.listen(app);
 
 sio.set('authorization', function (data, accept) {
-    if (data.headers.cookie) {
-        data.cookie = parseCookie(data.headers.cookie);
-        data.sessionID = data.cookie['express.sid'];
-        // save the session store to the data object
-        // (as required by the Session constructor)
-        data.sessionStore = sessionStore;
-        sessionStore.get(data.sessionID, function (err, session) {
-            if (err || !session) {
-                accept('Error', false);
-            } else {
-                // create a session object, passing data as request and our
-                // just acquired session data
-                data.session = new Session(data, session);
-                accept(null, true);
-            }
-        });
-    } else {
-       return accept('No cookie transmitted.', false);
+  /* NOTE: To detect which session this socket is associated with,
+  * we need to parse the cookies. */
+  if (!data.headers.cookie) {
+    return accept('Session cookie required.', false);
+  }
+  /* XXX: Here be hacks! Both of these methods are part of Connect's
+  * private API, meaning there's no guarantee they won't change
+  * even on minor revision changes. Be careful (but still
+  * use this code!) */
+  /* NOTE: First parse the cookies into a half-formed object. */
+  console.log(data.headers.cookie);
+  data.cookie = cookie.parse(data.headers.cookie);
+  console.log(data.cookie);
+  /* NOTE: Next, verify the signature of the session cookie. */
+  if(secret) {
+    data.cookie = connect.utils.parseSignedCookies(data.cookie, secret);
+    console.log(data.cookie);
+  }
+  // data.cookie = utils.parseJSONCookies(data.cookie);
+
+  /* NOTE: save ourselves a copy of the sessionID. */
+  data.sessionID = data.cookie['express.sid'];
+  console.log(data.sessionID, 'sid');
+  /* NOTE: get the associated session for this ID. If it doesn't exist,
+  * then bail. */
+  // data.sessionStore = sessionStore;
+  sessionStore.get(data.sessionID, function (err, session) {
+    if (err) {
+      return accept('Error in session store.', false);
+    } else if (!session) {
+      return accept('Session not found.', false);
     }
+    // create a session object, passing data as request and our
+    // just acquired session data
+    data.session = session;
+    return accept(null, true);
+  });
 });
 
 game_server.init(sio);
